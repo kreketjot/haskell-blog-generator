93c34d838e95d96fa39bb2e3f80bfae2b72c5272
Andrey Vinokurov <kreketjot@gmail.com>
2023-12-09 13:21:27 +0300

    3.4 Safer HTML construction with types

--- hello.hs
+++ hello.hs
@@ -1,31 +1,54 @@
-main = putStrLn myhtml
+main :: IO ()
+main = putStrLn (render myhtml)
 
-myhtml :: String
+myhtml :: Html
 myhtml =
-  makeHtml
+  html_
     "Hello title"
-    (h1_ "Hello, world!" <> p_ "Some text" <> p_ "Another paragraph")
+    ( append_
+        (h1_ "Hello, world!")
+        ( append_
+            (p_ "Some text")
+            (p_ "Another paragraph")
+        )
+    )
 
-makeHtml :: String -> String -> String
-makeHtml title content = html_ (head_ (title_ title) <> body_ content)
+newtype Html = Html String
 
-html_ :: String -> String
-html_ = el "html"
+newtype Structure = Structure String
 
-body_ :: String -> String
-body_ = el "body"
+type Title = String
 
-head_ :: String -> String
-head_ = el "head"
+html_ :: Title -> Structure -> Html
+html_ title (Structure content) =
+  Html
+    ( el
+        "html"
+        ( el
+            "head"
+            (el "title" title)
+            <> el "body" content
+        )
+    )
 
-title_ :: String -> String
-title_ = el "title"
+p_ :: String -> Structure
+p_ = Structure . el "p"
 
-p_ :: String -> String
-p_ = el "p"
-
-h1_ :: String -> String
-h1_ = el "h1"
+h1_ :: String -> Structure
+h1_ = Structure . el "h1"
 
 el :: String -> String -> String
 el tag content = "<" <> tag <> ">" <> content <> "</" <> tag <> ">"
+
+append_ :: Structure -> Structure -> Structure
+append_ (Structure a) (Structure b) = Structure (a <> b)
+
+getStructureString :: Structure -> String
+getStructureString struct =
+  case struct of
+    Structure str -> str
+
+render :: Html -> String
+render html =
+  case html of
+    Html str -> str
